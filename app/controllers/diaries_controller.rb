ebdc9356e902b2a09b9ab1079b518411c262ade5
PianoFF <Fangfei1star@gmail.com>
2020-01-20 10:59:52 +0000

    diary controller _ create

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -1,2 +1,12 @@
 class DiariesController < ApplicationController
+
+    def create 
+        diary = Diary.create(diary_params)
+    end 
+
+    private
+
+    def diary_params
+        params.require(:diary).permit(:user_id)
+    end
 end
