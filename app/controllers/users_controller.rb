220d7a104a440913a6b0e67e29454a428cb5e214
PianoFF <Fangfei1star@gmail.com>
2020-01-20 09:26:19 +0000

    user controller & routes _ login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,16 @@ class UsersController < ApplicationController
         end
     end
 
+    def login
+        user = User.find_by(email: user_params[:email])
+
+        if user && user.authenticate(user_params[:password])
+            render json: user
+        else
+            render json: {errors: ["Whoopsie, something's gong wrong"]}, status: :unauthorized
+        end
+    end
+
     private
     def user_params
         params.require(:user).permit(:username, :password, :password_confirmation, :email)
