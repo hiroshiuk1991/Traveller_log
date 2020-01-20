dc169deaf258dd8b69d9551c702406ba5af13a3b
PianoFF <Fangfei1star@gmail.com>
2020-01-20 09:15:57 +0000

    user controller _ create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+
+    def create
+        user = User.create(user_params)
+        
+        if user.valid? 
+            render json: user
+        else
+            render json: {errors: user.errors.full_messages}, status: :unauthorized
+        end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:username, :password, :password_confirmation, :email)
+    end
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :email, :password_digest
+  attributes :id, :username, :email
 
   has_many :diaries
   has_many :diary_entries, through: :diaries
